6b245126405f43b076e12b53b815c46ab85c0b29
Amal <amaldev.psn@gmail.com>
2023-12-04 01:13:45 +0530

    c

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { OnInit } from '@angular/core';
+import { finalize, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  title = 'Sample title';
+  constructor(private http: HttpClient) { }
+  ngOnInit() {
+
+    this.yourApiCall();
+  }
+
+  yourApiCall() {
+    this.http.get('https://fakestosreapi.com/products/1')
+      .pipe(
+        catchError((error: any) => {
+          alert('error');
+          return of(null); // Emitting null to proceed to finalize
+        }),
+        finalize(() => {
+          alert('final');
+        })
+      )
+      .subscribe((response: any) => {
+        if (response !== null) {
+          alert('success');
+        }
+      });
+  }
+
 }
+
