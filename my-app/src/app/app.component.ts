6505b45be135aa111cf94dc3e0fa0fddacf052dc
Amal <amaldev.psn@gmail.com>
2023-12-04 01:58:48 +0530

    c

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { OnInit } from '@angular/core';
-import { finalize, catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -12,17 +13,45 @@ import { of } from 'rxjs';
 export class AppComponent implements OnInit {
   title = 'Sample title';
   data = '';
-  constructor(private http: HttpClient) { }
-  ngOnInit() {
-    this.yourApiCall();
+  input = '';
+  count = 0;
+  private searchTerms = new Subject<string>();
+  ngOnInit(): void {
+
+  }
+  constructor(private http: HttpClient) {
+    this.searchTerms.pipe(
+      // Add a delay of 300ms between calls
+      debounceTime(300),
+      // Ignore if the next term is the same as the previous one
+      distinctUntilChanged(),
+      // Switch to a new observable each time the term changes
+      switchMap((term: string) => this.searchApiCall(term))
+    ).subscribe((result) => {
+      // Handle the API call result here
+      console.log(result);
+    });
   }
 
-  yourApiCall() {
-    this.http.get('https://fakestoreapi.com/products/1')
-      .subscribe((response: any) => {
-        this.data = response;
-      });
+
+  // Function to push search terms into the subject
+  search(term: any): void {
+    const val = term.target.value;
+    this.searchTerms.next(val);
   }
+
+  // API call function
+  searchApiCall(term: string): Observable<any> {
+    const apiUrl = `https://fakestoreapi.com/products/1`;
+    return this.http.get(apiUrl);
+  }
+
+  /*  yourApiCall() {
+     this.http.get('https://fakestoreapi.com/products/1')
+       .subscribe((response: any) => {
+         this.data = response;
+       });
+   } */
 
 }
 
