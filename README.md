701fdc985303826b139a6baf8c1f90d70f8b99fc
Amal <amaldev.psn@gmail.com>
2023-12-09 00:31:29 +0530

    c

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Created this repo for all the angular play arounds
 create a branch from main branch for all the play arounds
 dont touch on main branch.
+----------------
+Using a JavaScript library (without type declarations) in a TypeScript project.
+
+https://medium.com/@steveruiz/using-a-javascript-library-without-type-declarations-in-a-typescript-project-3643490015f3
--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Tableau from 'tableau-api-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'my-app';
+
+  public ngOnInit(): void {
+    this.initViz();
+  }
+  public initViz(): void {
+    const containerDiv = document.getElementById('vizContainer');
+    const vizUrl = 'https://public.tableau.com/views/WorldIndicators/GDPpercapita';
+    const options = {
+      width: containerDiv && containerDiv.offsetWidth,
+      height: 700
+    };
+
+    const viz = new Tableau.Viz(containerDiv, vizUrl, options);
+  }
 }
