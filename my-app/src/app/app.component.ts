f547ae773b34d3357c685ee815222f04eaff63a4
Amaldev <amaldev.psn@gmail.com>
2025-02-05 23:30:32 +0530

    c

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+export class RegionCountry {
+  constructor(public region: string, public country: string) { }
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  regionCountries: RegionCountry[] = [
+    { "region": "Asia", "country": "India" },
+    { "region": "Asia", "country": "China" },
+    { "region": "Asia", "country": "Japan" },
+    { "region": "Asia", "country": "South Korea" },
+    { "region": "Asia", "country": "Thailand" },
+    { "region": "Asia", "country": "Vietnam" },
+    { "region": "Asia", "country": "Indonesia" },
+
+    { "region": "America", "country": "United States" },
+    { "region": "America", "country": "Canada" },
+    { "region": "America", "country": "Brazil" },
+    { "region": "America", "country": "Mexico" },
+
+    { "region": "Europe", "country": "United Kingdom" },
+    { "region": "Europe", "country": "Germany" },
+    { "region": "Europe", "country": "France" },
+    { "region": "Europe", "country": "Italy" },
+
+    { "region": "Africa", "country": "Nigeria" },
+    { "region": "Africa", "country": "South Africa" },
+    { "region": "Africa", "country": "Egypt" },
+    { "region": "Africa", "country": "Kenya" }
+  ];
+
+
+  displayedColumns: string[] = ['region', 'country'];
+
+  dataSource = new MatTableDataSource(this.regionCountries);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  shouldShowRegion(index: number): boolean {
+    if (index === 0) return true;
+    return this.regionCountries[index].region !== this.regionCountries[index - 1].region;
+  }
+
+  getRowSpan(index: number): number {
+    const currentRegion = this.regionCountries[index].region;
+    let rowSpan = 1;
+
+    for (let i = index + 1; i < this.regionCountries.length; i++) {
+      if (this.regionCountries[i].region === currentRegion) {
+        rowSpan++;
+      } else {
+        break;
+      }
+    }
+
+    return rowSpan;
+  }
 }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
