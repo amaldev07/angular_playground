fe0c3073aa1690ac1fabb9406c00fec0a77b9760
Amaldev <amaldev.psn@gmail.com>
2025-03-28 01:07:10 +0530

    added ngx logger but getting cors error

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NGXLogger } from 'ngx-logger';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private logger: NGXLogger, private http: HttpClient) { }
+
+
   title = 'my-app';
+  ngOnInit() {
+    this.logger.info('AppComponent initialized');
+    this.someMethod();
+    this.callSampleGetApi();
+  }
+
+  someMethod() {
+    this.logger.debug('Debugging info');
+    this.logger.info('Useful information');
+    this.logger.warn('Warning information');
+    this.logger.error('Error encountered!');
+  }
+  callSampleGetApi() {
+    this.http.get('http://localhost:8080/employees').subscribe(
+      (response) => {
+        this.logger.info('GET request successful', response);
+      },
+      (error) => {
+        this.logger.error('GET request failed', error);
+      }
+    );
+  }
 }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { HttpClientModule } from "@angular/common/http";
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
+    LoggerModule.forRoot({
+      level: NgxLoggerLevel.DEBUG, // or INFO, ERROR as needed
+      serverLogLevel: NgxLoggerLevel.ERROR, // Level for sending logs to server
+      serverLoggingUrl: 'http://localhost:8080/api/logs' // Endpoint to your backend
+    }),
     BrowserModule,
     AppRoutingModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
